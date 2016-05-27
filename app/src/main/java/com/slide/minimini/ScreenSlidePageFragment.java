cce9abb0ffdd7c847e3d5473255fcbc4d5e95fff
Cnc <cristian_dev@pmovil.com.ar>
2016-05-27 11:50:09 -0300

    Initial commit

---
+++ app/src/main/java/com/slide/minimini/ScreenSlidePageFragment.java
@@ -0,0 +1,23 @@
+package com.slide.minimini;
+
+/**
+ * Created by Cnc on 27/05/2016.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ScreenSlidePageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_screen_slide_page, container, false);
+
+        return rootView;
+    }
+}
